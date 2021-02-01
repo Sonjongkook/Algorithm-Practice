7b5add5dd4a67833d6e62ce33eac6a043e664423
Kyle Son <sjk378@gmail.com>
2021-01-31 18:33:09 -0600

    Update Solution.java

--- LinkedListConstruction/Solution.java
+++ LinkedListConstruction/Solution.java
@@ -11,40 +11,94 @@ class Program {
 				head = node;
 				tail = node;
 				return;
-
 			} 
-			System.out.println(node.value);
-			head.prev = node;
-			node.next = head;
-			head = node;
-      // Write your code here.
+			insertBefore(head, node);
+
     }
 
     public void setTail(Node node) {
       // Write your code here.
-			tail.next = node;
-			node.prev = tail;
-			tail = node;
+			if(tail == null){
+				setHead(node);
+				return;
+
+			}
+			insertAfter(tail, node);
     }
 
     public void insertBefore(Node node, Node nodeToInsert) {
       // Write your code here.
+			if(nodeToInsert==head && nodeToInsert==tail)return;
+			remove(nodeToInsert);
+			nodeToInsert.prev = node.prev;
+			nodeToInsert.next = node;
+			if(node.prev == null){
+				head = nodeToInsert;
+			}else{
+				node.prev.next = nodeToInsert;
+
+			}
+			node.prev = nodeToInsert;
+			
+			
     }
 
     public void insertAfter(Node node, Node nodeToInsert) {
       // Write your code here.
+			if(nodeToInsert==head && nodeToInsert==tail)return;
+			remove(nodeToInsert);
+			nodeToInsert.prev = node;
+			nodeToInsert.next = node.next;
+			if (node.next == null){
+					tail = nodeToInsert;
+			}else{
+				node.next.prev = nodeToInsert;
+
+			}
+			node.next = nodeToInsert;
+			
     }
 
     public void insertAtPosition(int position, Node nodeToInsert) {
       // Write your code here.
-    }
+			if(position == 1){
+				setHead(nodeToInsert);
+				return;
+
+			}
+			Node node = head;
+			int cur_postion = 1;
+			while(node != null && cur_postion++ !=  position){
+				node = node.next;
+			}
+			if(node != null){
+				insertBefore(node, nodeToInsert);
+			}else{
+				setTail(nodeToInsert);
+			}
+		}
 
     public void removeNodesWithValue(int value) {
       // Write your code here.
-    }
+			Node node = head;
+			while(node!= null){
+					Node nodeToRemove = node;
+					node = node.next;
+					if(nodeToRemove.value == value){
+							remove(nodeToRemove);
+					}
+			}
+		}
 
     public void remove(Node node) {
-      // Write your code here.
+			if(node == head) {
+					head = head.next;
+			}
+			if(node == tail){
+					tail = tail.prev;
+			}
+			Breakbind(node);
+			// Write your code here.
     }
 
     public boolean containsNodeWithValue(int value) {
@@ -53,9 +107,22 @@ class Program {
 			while(node != null && node.value != value){
 				node = node.next;
 			}
-			return false;
+			//If node value is not null, return true 			
+			return node != null;
     }
-  }
+		
+		public void Breakbind(Node node){
+			if(node.prev != null){
+				node.prev.next = node.next;
+			}
+			if(node.next != null){
+				node.next.prev = node.prev;
+			}
+			node.prev = null;
+			node.next = null;
+
+		}
+	}
 
   // Do not edit the class below.
   static class Node {
